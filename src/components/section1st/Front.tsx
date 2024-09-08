5da90a561a32d39b3448d7d83634e16777ac68e7
sahil_saif <sahilsaif2002@gmail.com>
2024-09-08 19:26:18 +0530

    commit

--- src/components/section1st/Front.tsx
+++ src/components/section1st/Front.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './Card'; 
 import {  useNavigate } from 'react-router-dom';
 import alertIcon from '../../assets/AlertTriangle.svg'
-import iIcon from '../../assets/iIcon.svg'
+import iIcon from '../../assets/Icon.png'
 import { useToast } from '@/hooks/use-toast';
 
 const Front: React.FC = () => {
